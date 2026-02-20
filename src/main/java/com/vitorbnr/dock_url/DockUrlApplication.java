e9dd5e798a699a7855a0eb0750c12cbfc28b3dfa
vitorbnr <vitorbernardotech@gmail.com>
2026-02-20 11:49:52 -0300

    configuração inicial do projeto

---
+++ src/main/java/com/vitorbnr/dock_url/DockUrlApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.dock_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockUrlApplication.class, args);
+	}
+
+}
